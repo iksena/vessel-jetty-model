bc511a488f331ebc46ba7eee1b5185dde8de55ae
I KOMANG SENA <i.buwana@btpn.com>
2021-10-10 14:16:55 +0700

    Add preview component

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,7 @@
-import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei'
-
-const Box = (props) => {
-  const ref = useRef();
-
-  return (
-    <mesh
-      position={[0, 0, 0]}
-      ref={ref}
-      {...props}
-      >
-      <boxGeometry args={[7, 7, 7]} />
-      <meshStandardMaterial color={'orange'} />
-    </mesh>
-  )
-}
+import Preview from '../components/preview';
 
 export default function Home() {
   return (
-    <Canvas>
-      <OrbitControls enablePan/>
-      <Box position={[-136, 0, -28.5]} />
-      <Box position={[-45, 0, 0]} />
-      <Box position={[-22.5, 0, 0]} />
-      <Box position={[22.5, 0, 0]} />
-      <Box position={[45, 0, 0]} />
-      <Box position={[119, 0, -35]} />
-    </Canvas>
+    <Preview />
   )
 }

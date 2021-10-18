12dec907b6097955775510ddcdefcfae20692862
I KOMANG SENA <i.buwana@btpn.com>
2021-10-18 20:58:39 +0700

    Tryng to fix path

--- components/index.js
+++ components/index.js
@@ -2,7 +2,7 @@ export { default as BreastingDolphin } from './BreastingDolphin';
 export { default as MooringDolphin } from './MooringDolphin';
 export { default as MooringLine } from './MooringLine';
 export { default as Platform } from './Platform';
-export { default as Preview } from './Preview';
+export { default as Preview } from './preview';
 export { default as ResultsTable } from './ResultsTable';
 export { default as Ship } from './Ship';
 export { default as Vessel } from './Vessel';
